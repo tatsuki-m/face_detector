e0b39451bdf3197cf45f5002b4468d403d41e517
Yuta Ide <yIde4949@gmail.com>
2017-04-29 19:35:09 +0900

    [update] send wav file

--- RecordVoice.py
+++ RecordVoice.py
@@ -4,18 +4,25 @@ import subprocess
 from datetime import datetime
 
 # 音声をレコードするためのクラス
-class RecordVoice():
+class RecordVoice:
     def __init__(self):
         self.isRecording = 0
         print("init")
+        
     def startRecording(self):
         self.isRecording = 1
-        fileName = datetime.now().strftime('%Y%m%d%H%M%S')
+        fileName = "a.wav"
         # subprocess.call("arecord -f cd -c 2 -D dsnoop " + fileName, shell=True)
-        subprocess.call("touch " + fileName, shell=True)
+        subprocess.call("touch " + fileName, shell=True) # ファイル書き出し(テスト)
+        
     def endRecording(self):
         print("send voice to server")
         # subprocess.call("pkill arecord")
+        
+        # ここでwav消す前に送る
+        cmd = 'curl -X POST -u 85575991-f440-415f-90a8-1cdca5b16f84:O7i5matSrnoC --header "Content-Type: audio/wav" --header "Transfer-Encoding: chunked" --data-binary a.wav "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&word_alternatives_threshold=0.9&keywords=%22colorado%22%2C%22tornado%22%2C%22tornadoes%22&keywords_threshold=0.5&continuous=true&model=ja-JP_BroadbandModel"'
+        subprocess.call(cmd, shell=True)
+        
         print("remove wav from dir")
-        # subprocess.call("rm *.wav")
+        subprocess.call("rm a.wav")
         self.isRecording = 0
